05166909dd10702b0aeab8e66c901f4b0cbe7644
ajmal <ajmal@DESKTOP-CHPVC98>
2018-03-15 22:00:10 +0400

    prototype menu 1

--- src/AddCar.java
+++ src/AddCar.java
@@ -1,10 +1,17 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class AddCar {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -34,8 +41,40 @@ public class AddCar {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 755, 534);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(327, 62, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(327, 106, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(327, 152, 86, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(327, 202, 86, 20);
+		frame.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(327, 252, 86, 20);
+		frame.getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setText("00");
+		textField_5.setBounds(327, 296, 86, 20);
+		frame.getContentPane().add(textField_5);
+		textField_5.setColumns(10);
 	}
 
 }
--- src/CarReg.java
+++ src/CarReg.java
@@ -1,6 +1,13 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
 
 public class CarReg {
 
@@ -36,6 +43,28 @@ public class CarReg {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 851, 561);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.setBounds(346, 311, 124, 23);
+		frame.getContentPane().add(btnSearch);
+		
+		JButton btnUpdate = new JButton("Update ");
+		btnUpdate.setBounds(346, 362, 124, 23);
+		frame.getContentPane().add(btnUpdate);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setBounds(346, 193, 124, 23);
+		frame.getContentPane().add(btnAdd);
+		
+		JButton btnSort = new JButton("Sort");
+		btnSort.setBounds(346, 256, 124, 23);
+		frame.getContentPane().add(btnSort);
+		
+		JLabel lblCarRegistration = new JLabel("Car Registration");
+		lblCarRegistration.setBounds(359, 44, 94, 14);
+		frame.getContentPane().add(lblCarRegistration);
+		
+		
 	}
-
 }
